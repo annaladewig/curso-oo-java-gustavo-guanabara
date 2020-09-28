a7e81022796eea47b432cdb8118adf4336673ebf
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-09-28 19:40:08 -0300

    Adicionei  PrimeiroPrograma

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeiroprograma;
+
+/**
+ *
+ * @author DELL
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Olá, Mundo!"); 
+        /**sout + TAB -> Já escreve System.out.prinln na tela. Usa o println, ou seja, pula uma linha.
+         * psvm + TAB -> Já escreve public satatic void main(String[] args) na tela.
+         */
+            
+    }
+}
