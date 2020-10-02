07c002ec2a55f05e659bc68200b69c0aec3d58cb
Anna Ladewig <annamaria.ladewig@gmail.com>
2020-10-02 15:59:32 -0300

    Adicionei Aula 05 partes 1 e 2

---
+++ Aula05(Parte2)/src/aula05/parte2/FXMLDocumentController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05.parte2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author DELL
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblMensagem;
+    private Button btnClick;
+    
+    @FXML
+    private void clicouBotao(ActionEvent event) {
+        lblMensagem.setText("Agora é Java!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
